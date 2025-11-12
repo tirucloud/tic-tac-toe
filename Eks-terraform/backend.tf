47117c6a09d7f7600dfd0e63a76aeb15fc71b7f1
tirucloud <tirucloud@gmail.com>
2025-11-12 09:29:31 +0530

    allll

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tiru-123489" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "tic-tac-toe/terraform.tfstate"
     region = "us-east-1"
   }
 }
